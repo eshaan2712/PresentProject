9a1f4f705eac864d3ecf6a0b1474504b3a26875a
Eshaan, K <eshaan.katla@gmail.com>
2024-11-19 10:57:22 +0530

    Demo Project for team

---
+++ src/main/java/com/app/demo/PresentProject/App.java
@@ -0,0 +1,11 @@
+package com.app.demo.PresentProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Eshaan!");
+        System.out.println("This is your Maven Project using Jenkins");
+        }
+}
